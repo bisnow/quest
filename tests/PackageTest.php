95de290e16141c0147822d7876ebf49e946bfdc7
Wes Hulette <wes.hulette@bisnow.com>
2025-01-30 10:47:19 -0500

    Updated testing matrix

--- tests/PackageTest.php
+++ tests/PackageTest.php
@@ -21,9 +21,9 @@ class PackageTest extends TestCase
             'url'            => env('DATABASE_URL'),
             'host'           => env('DB_HOST', '127.0.0.1'),
             'port'           => env('DB_PORT', 3306),
-            'database'       => env('DB_DATABASE', 'testing'),
+            'database'       => env('DB_DATABASE', 'quest'),
             'username'       => env('DB_USERNAME', 'root'),
-            'password'       => env('DB_PASSWORD', ''),
+            'password'       => env('DB_PASSWORD', 'root'),
             'unix_socket'    => env('DB_SOCKET', ''),
             'charset'        => 'utf8mb4',
             'collation'      => 'utf8mb4_unicode_520_ci',
@@ -46,8 +46,6 @@ class PackageTest extends TestCase
         DB::table('users')->insert(['name' => 'Jane Doe', 'nickname' => 'jndoe', 'country' => 'United Kingdom']);
         DB::table('users')->insert(['name' => 'Fred Doe', 'nickname' => 'fredrick', 'country' => 'France']);
         DB::table('users')->insert(['name' => 'William Doe', 'nickname' => 'willy', 'country' => 'Italy']);
-
-
     }
 
     /** @test */
